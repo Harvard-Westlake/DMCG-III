81a8cae9f045f0f08bb049cce3f544ebac08a6b0
mateensharifi <mateensharifi11@gmail.com>
2022-02-22 14:19:44 -0800

    Added instance variables

---
+++ bigCheese.js
@@ -0,0 +1,16 @@
+class bigCheese {
+  let splitArr [];
+  let isAlive = true;
+  let jumpCooldown;
+  let canJump;
+  let isSplit = false;
+  let xPos;
+  let yPos;
+  let width;
+  let height;
+  let xVelo;
+  let yVelo;
+  const gravity;
+  const friction;
+  let isMoving = false;
+}
