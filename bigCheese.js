643c05ff76a099e8e97768bfbdc2d617cbd4f161
mateensharifi <mateensharifi11@gmail.com>
2022-02-22 14:23:43 -0800

    Started move()

--- bigCheese.js
+++ bigCheese.js
@@ -13,4 +13,24 @@ class bigCheese {
   const gravity;
   const friction;
   let isMoving = false;
+
+  function move(String direction) {
+    if (document.getElementById(87).addEventListener("click", function()) && this.isAlive == true && this.isMoving = true) {
+      yPos = yPos - 1;
+      canJump = false;
+      yVelo = yVelo - (yVelo * 2); // set to "specific value"
+      // every second add yVelo to yPos of bigCheese
+      // yVelo increases due to the acceleration of gravity until the cheese lands or collides
+    }
+    if (document.getElementById(65).addEventListener("click", function()) && this.isAlive == true && this.isMoving = true) {
+      xVelo = xVelo - (xVelo * 2); // set to "specific value"
+      // every second add xVelo to xPos of bigCheese
+    }
+      if (document.getElementById(68).addEventListener("click", function()) && this.isAlive == true && this.isMoving = true) {
+        xVelo = Math.abs(xVelo); // set to "specific value"
+        // every second add xVelo to xPos of bigCheese
+      }
+    checkForCollision();
+    checkForLand();
+  }
 }
