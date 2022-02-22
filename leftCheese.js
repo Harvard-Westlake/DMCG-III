048644cad4835f59abf85b6a1aac6e1aec92d3a8
mateensharifi <mateensharifi11@gmail.com>
2022-02-21 20:16:17 -0800

    Added lateral movement

--- leftCheese.js
+++ leftCheese.js
@@ -1,7 +1,19 @@
 class leftCheese extends BigCheese {
   function connectLeft() {
-    if (document.getElementById(49).addEventListener("click", function() && this.isAlive == true) {
+    if (document.getElementById(49).addEventListener("click", function()) && this.isAlive == true) {
       this.summonCheese(49);
+    }
+  }
+
+  function moveLeft() {
+    if (document.getElementById(65).addEventListener("click", function()) && this.isAlive == true) {
+      Move("left");
+    }
+  }
+
+  function moveRight() {
+    if (document.getElementById(68).addEventListener("click", function()) && this.isAlive == true) {
+      Move("right");
     }
   }
 }
