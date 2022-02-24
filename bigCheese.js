d700b82ad003a9284163be5a0f92419845d721a3
mateensharifi <mateensharifi11@gmail.com>
2022-02-24 13:14:49 -0800

    Fixed variables

--- bigCheese.js
+++ bigCheese.js
@@ -1,19 +1,19 @@
 class bigCheese {
-  let splitArr [];
-  let isAlive = true;
-  let jumpCooldown = 0.5;
-  let canJump;
-  let isSplit = false;
-  let xPos;
-  let yPos;
-  let width;
-  let height;
-  let xVelo;
-  let yVelo;
-  const gravity = 9.81;
-  const friction;
-  let isMoving = false;
-  let gForce;
+  let splitArr: [],
+  let isAlive : true,
+  let jumpCooldown : 0.5,
+  let canJump : true,
+  let isSplit : false,
+  let xPos: 0,
+  let yPos : 0,
+  let width : 10,
+  let height : 10,
+  let xVelo : 0,
+  let yVelo : 0,
+  const gravity : 9.81,
+  const friction : 5,
+  let isMoving : false,
+  let gForce : 0,
 
   function move(String direction) {
     while (document.getElementById(87).addEventListener("click", function()) && this.isAlive == true && this.isMoving = true) {
