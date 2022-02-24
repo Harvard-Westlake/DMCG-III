d74aa024a9b198c98e7e875fdd5dfc7428ee1883
mateensharifi <mateensharifi11@gmail.com>
2022-02-23 22:22:58 -0800

    Added setInterval and helper methods for move

--- bigCheese.js
+++ bigCheese.js
@@ -10,28 +10,49 @@ class bigCheese {
   let height;
   let xVelo;
   let yVelo;
-  const gravity;
+  const gravity = 9.81;
   const friction;
   let isMoving = false;
+  let gForce;
 
   function move(String direction) {
     if (document.getElementById(87).addEventListener("click", function()) && this.isAlive == true && this.isMoving = true) {
       yPos = yPos - 1;
       canJump = false;
-      yVelo = yVelo - (yVelo * 2); // set to "specific value"
-      // every second add yVelo to yPos of bigCheese
-      // yVelo increases due to the acceleration of gravity until the cheese lands or collides
+      yVelo++;
+      yVelo = yVelo - (yVelo * 2);
+      let t = setInterval(addYVelo(), 1000);
+      clearInterval(t);
+      while (isMoving = false || checkForCollision().equals(false)) {
+        let a = setInterval(gravitationalForce(), 1000);
+      }
     }
     if (document.getElementById(65).addEventListener("click", function()) && this.isAlive == true && this.isMoving = true) {
-      xVelo = xVelo - (xVelo * 2); // set to "specific value"
-      // every second add xVelo to xPos of bigCheese
+      xVelo++;
+      xVelo = xVelo - (xVelo * 2);
+      let t = setInterval(addXVelo(), 1000);
+      clearInterval(t);
     }
       if (document.getElementById(68).addEventListener("click", function()) && this.isAlive == true && this.isMoving = true) {
-        xVelo = Math.abs(xVelo); // set to "specific value"
-        // every second add xVelo to xPos of bigCheese
+        xVelo++;
+        xVelo = Math.abs(xVelo);
+        let t = setInterval(addXVelo(), 1000);
+        clearInterval(t);
       }
     checkForCollision();
     checkForLand();
+  }
+
+  function addYVelo() {
+    yPos = yPos + yVelo;
+  }
+
+  function gravitationalForce() {
+    gForce = yVelo + gravity;
+  }
+
+  function addXVelo() {
+    xPos = xPos + xVelo;
   }
 
   function landOnPlatform() {
