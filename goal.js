bb6555755413a15634e88ea869167000600c8719
DidSomeoneSayBoba <aureliadufromage37@gmail.com>
2022-02-24 14:07:08 -0800

    goal made yes?

---
+++ goal.js
@@ -0,0 +1,13 @@
+class Goal{
+  xpos;
+  ypos;
+  width;
+  height;
+  texture = doorTexture;
+  constructor(x,y,w,h){
+    this.xpos = x;
+    this.ypos = y;
+    this.width = w;
+    this.height =h;
+  }
+}
