b8db378b9a160901b886cfa5a7b2dfaef5923a75
mateensharifi <mateensharifi11@gmail.com>
2022-02-24 13:56:41 -0800

    Combined to bigCheese

--- bigCheese.js
+++ bigCheese.js
@@ -21,23 +21,23 @@ class bigCheese {
       canJump = false;
       yVelo++;
       yVelo = yVelo - (yVelo * 2);
-      let t = setInterval(addYVelo(), 1000);
+      let t = setInterval(addYVelo(), 16.6666666667);
       clearInterval(t);
       while (isMoving = false || checkForCollision().equals(false)) {
-        let a = setInterval(gravitationalForce(), 1000);
+        let a = setInterval(gravitationalForce(), 16.6666666667);
       }
       clearInterval(t);
     }
     if (document.getElementById(65).addEventListener("click", function()) && this.isAlive == true && this.isMoving = true) {
       xVelo++;
       xVelo = xVelo - (xVelo * 2);
-      let t = setInterval(addXVelo(), 1000);
+      let t = setInterval(addXVelo(), 16.6666666667);
       clearInterval(t);
     }
       if (document.getElementById(68).addEventListener("click", function()) && this.isAlive == true && this.isMoving = true) {
         xVelo++;
         xVelo = Math.abs(xVelo);
-        let t = setInterval(addXVelo(), 1000);
+        let t = setInterval(addXVelo(), 16.6666666667);
         clearInterval(t);
       }
     checkForCollision();
@@ -66,7 +66,7 @@ class bigCheese {
     }
     isMoving = false;
     while (jumpCooldown > 0) {
-      let j = setInterval(editJumpCooldown(), 1000);
+      let j = setInterval(editJumpCooldown(), 16.6666666667);
     }
     clearInterval(j);
     canJump = true;
@@ -75,4 +75,64 @@ class bigCheese {
   editJumpCooldown() {
     jumpCooldown = jumpCooldown - 0.1;
   }
-}
+  split() {
+    if(jumpCooldown == true) return;
+    isSplit = true;
+    //https://stackoverflow.com/questions/9419263/how-to-play-audio
+    var audio = new Audio('Sometimes i dream about cheese Sound Effect (Clean).mp3');//which audio file is it supposed to be?
+    audio.play();
+    LeftCheese left = new LeftCheese();//need velo inputs?
+    MiddleCheese middle = new MiddleCheese();
+    RightCheese right = new RightCheese();
+    splitArr[0] = left;
+    splitArr[1] = middle;
+    splitArr[2] = right;
+  }
+
+  //https://stackoverflow.com/questions/26269433/check-javascript-condition-every-frame/26269529
+  window.onload = function() {
+      checkForCollision(platforms, traps, lava) {
+         while(isMoving){
+           for(int i = 0; i < platforms.length; i++){
+             if(xPos == platforms[i].getXPos() && yPos == platforms[i].getYPos()){
+               die();
+               return;
+             }
+           }
+           for(int i = 0; i < traps.length; i++){
+             if(xPos == traps[i].getXPos() && yPos == traps[i].getYPos()){
+               die();
+               return;
+             }
+           }
+           for(int i = 0; i < lava.length; i++){
+             if(xPos == lava[i].getXPos() && yPos == lava[i].getYPos()){
+               die();
+               return;
+             }
+           }
+
+       }
+       setInterval(checkForCollision, 16.6666666667); //(method, time in milliseconds)
+   }
+  }
+  window.onload = function() {
+      checkForLand(platforms, traps, lava) {
+         while(isMoving){
+           for(int i = 0; i < platforms.length; i++){
+             if(platforms[i].inLandingRange(xPos, yPos)){
+               land();
+               return;
+             }
+           }
+       }
+       setInterval(checkForLand, 16.6666666667); //(method, time in milliseconds)
+   }
+  }
+
+  die(){
+    isAlive = false;
+    //micah figure out ur sh*t bro
+  }
+
+  }
